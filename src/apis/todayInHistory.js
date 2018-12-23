9a7fd9d26ec7af3dabf0c8d7de1c6cbaa5cbc5b7
jwkim960315 <jkim11@wesleyan.edu>
2018-12-23 22:57:54 +0900

    Working on refactoring

--- src/apis/todayInHistory.js
+++ src/apis/todayInHistory.js
@@ -2,7 +2,5 @@ import axios from 'axios';
 
 
 export default axios.create({
-	baseURL: 'https://history.muffinlabs.com/date',
-	withCredentials: false,
-	'Access-Control-Allow-Origin': '*'
+	baseURL: 'https://history.muffinlabs.com/date'
 });
